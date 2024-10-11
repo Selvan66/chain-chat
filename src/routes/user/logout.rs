8a243fa88d76e58ca62d367e298b07422a51327b
Selvan66 <rafikop.pl@gmail.com>
2024-10-11 17:20:52 +0200

    add logs to logout

--- src/routes/user/logout.rs
+++ src/routes/user/logout.rs
@@ -1,11 +1,17 @@
+use actix_web::web;
 use actix_web::HttpResponse;
 
 use crate::domain::messages::*;
-use crate::session::UserSession;
+use crate::session::{UserId, UserSession};
 use crate::utils::see_other_with_flash;
 
+#[tracing::instrument(
+    skip_all,
+    fields(user_id=%&*user_id)
+)]
 #[actix_web::post("/logout")]
-pub async fn logout_post(session: UserSession) -> HttpResponse {
+pub async fn logout_post(session: UserSession, user_id: web::ReqData<UserId>) -> HttpResponse {
     session.log_out();
+    tracing::info!("Logout!");
     see_other_with_flash("/", LOGOUT_MESSAGE)
 }
