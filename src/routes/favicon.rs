b267687d26477da503dad22c5ca4b2881372e22c
Selvan66 <rafikop.pl@gmail.com>
2025-01-31 16:39:01 +0100

    update favicon get

--- src/routes/favicon.rs
+++ src/routes/favicon.rs
@@ -1,6 +1,6 @@
 use actix_files::NamedFile;
 
 #[actix_web::get("/favicon.ico")]
-pub async fn favicon_get() -> actix_web::Result<NamedFile> {
-    Ok(NamedFile::open("static/favicon.ico")?)
+pub async fn favicon_get() -> impl actix_web::Responder {
+    NamedFile::open_async("static/favicon.ico").await
 }
